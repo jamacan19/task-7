0bfa0f33857c50e5422f882d8af0e357b8a429de
jamacan19 <124731983+jamacan19@users.noreply.github.com>
2023-02-14 17:48:20 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
-    if (input.soundLevel() < 100) {
+    if (input.soundLevel() < 90) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, randint(0, 50))
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, randint(0, 50))
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, randint(0, 50))
         basic.pause(500)
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
